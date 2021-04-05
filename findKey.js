3f26a170f8a9d223bc6a97ec532d804ccc027a1e
Mohamed Hassan <mhass405@gmail.com>
2021-04-05 23:38:47 +0000

    refractored to work with mocha and chai

--- findKey.js
+++ findKey.js
@@ -1,48 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-let object1 = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-
-const eqObjects = function(object1, object2) {
-  const object1Length = Object.keys(object1).length;
-  const object2Length = Object.keys(object2).length;
-  
-  if (object1Length === object2Length) {
-    for (let key in object1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-};
-
-const assertObjectsEqual = function (actual, expected) {
-  const inspect = require('util').inspect;
-  if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅  Assertion passed: ${inspect(actual)} === ${inspect(expected)}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion failed: ${inspect(actual)} !== ${inspect(expected)}`);
-  }
-};
-
 const findKey = (object1, callback) => {
 
   for (let key in object1) {
@@ -58,4 +13,6 @@ const findKey = (object1, callback) => {
   return undefined;
 };
 
-assertObjectsEqual(findKey(object1, x => x.stars === 2), { stars: 2 });
+
+
+module.exports = findKey;
---
+++ test/findKeytest.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    let object1 = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    assert.deepEqual(findKey(object1, x => x.stars === 2), { stars: 2 });
+  });
+});
