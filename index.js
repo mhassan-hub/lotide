586f722f20992c334691e6cfa3a3859b0777052e
Mohamed Hassan <mhass405@gmail.com>
2021-04-05 21:47:50 +0000

    added a file to access all our lotide files

---
+++ index.js
@@ -0,0 +1,13 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertArraysEqual: assertArraysEqual
+};
